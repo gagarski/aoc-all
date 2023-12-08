ae7da4119e8b3f7f99a23f955fccb9eeeb45984b
Kirill Gagarski <kirill.gagarski@gmail.com>
2023-12-08 20:37:46 +0100

    2023 day 8 and 9

--- aoc-2023/src/main/kotlin/ski/gagar/aoc2023/Main.kt
+++ aoc-2023/src/main/kotlin/ski/gagar/aoc2023/Main.kt
@@ -1,11 +1,10 @@
 package ski.gagar.aoc2023
 
-import ski.gagar.aoc2023.day7.part1.day7Part1
-import ski.gagar.aoc2023.day7.part2.day7Part2
+import ski.gagar.aoc2023.day8.part2.day8Part2
 
 fun main() {
 //    day1Part1()
 //    day1Part2()
 //    day2Part2()
-    day7Part2()
+    day8Part2()
 }
