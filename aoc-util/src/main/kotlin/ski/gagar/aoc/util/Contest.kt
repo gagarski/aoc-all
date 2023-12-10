2f44c160158f5f2a66101bae7ac2c9e6a3163b26
Kirill Gagarski <kirill.gagarski@gmail.com>
2023-12-10 01:36:08 +0100

    m

--- aoc-util/src/main/kotlin/ski/gagar/aoc/util/Contest.kt
+++ aoc-util/src/main/kotlin/ski/gagar/aoc/util/Contest.kt
@@ -1,13 +1,24 @@
 package ski.gagar.aoc.util
 
+private const val PADDING = 10
+private const val SPACES = 2
+
+
 interface Contest {
     val puzzles: List<Puzzle>
         get() = listOf()
 
     fun run() {
-        println("======================")
-        println(this.javaClass.simpleName)
-        println("======================")
+        val name = this.javaClass.simpleName
+        println("=".repeat(PADDING * 2 + name.length))
+        println(
+            "*".repeat(PADDING - SPACES) +
+                    " ".repeat(SPACES) +
+                    name +
+                    " ".repeat(SPACES) +
+                    "*".repeat(PADDING - SPACES)
+        )
+        println("=".repeat(PADDING * 2 + name.length))
 
         for (puzzle in puzzles) {
             puzzle.run()
