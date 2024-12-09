4584f2d741596940db9f6e53eb7878053939c91b
Kirill Gagarski <kirill.gagarski@gmail.com>
2024-12-09 17:26:19 +0100

    Clean up 2015 before publishing

--- aoc-2015/src/main/kotlin/ski/gagar/aoc2015/day17/Day17.kt
+++ aoc-2015/src/main/kotlin/ski/gagar/aoc2015/day17/Day17.kt
@@ -1,15 +1,19 @@
 package ski.gagar.aoc2015.day17
 
 import ski.gagar.aoc.util.Puzzle
+import ski.gagar.aoc.util.getResourceAsStream
 import java.io.InputStream
 
 object Day17 : Puzzle {
     override val name: String = "No Such Thing as Too Much"
     override val inputPath: String = "/ski.gagar.aoc.aoc2015.day17/containers.txt"
+    val nPath: String = "/ski.gagar.aoc.aoc2015.day17/n.txt"
 
     override fun part1(input: InputStream) =
-        ski.gagar.aoc2015.day17.part1.nCombinations(input.bufferedReader().lineSequence())
+        ski.gagar.aoc2015.day17.part1.nCombinations(input.bufferedReader().lineSequence(),
+            getResourceAsStream(nPath).use { it.bufferedReader().readText().trim().toInt() })
 
     override fun part2(input: InputStream) =
-        ski.gagar.aoc2015.day17.part2.nCombinations(input.bufferedReader().lineSequence())
+        ski.gagar.aoc2015.day17.part2.nCombinations(input.bufferedReader().lineSequence(),
+            getResourceAsStream(nPath).use { it.bufferedReader().readText().trim().toInt() })
 }
