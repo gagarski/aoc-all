a2e8978fc8ed24b5e80b752563cbe9e04b8a2a83
Kirill Gagarski <kirill.gagarski@gmail.com>
2024-12-11 22:48:38 +0100

    m

--- aoc-2024/src/main/kotlin/ski/gagar/aoc2024/Aoc2024.kt
+++ aoc-2024/src/main/kotlin/ski/gagar/aoc2024/Aoc2024.kt
@@ -16,16 +16,16 @@ import kotlin.io.path.Path
 
 object Aoc2024 : Contest {
     override val puzzles = listOf(
-//        Day1,
-//        Day2,
-//        Day3,
-//        Day4,
-//        Day5,
-//        Day6,
-//        Day7,
-//        Day8,
-//        Day9,
-//        Day10,
+        Day1,
+        Day2,
+        Day3,
+        Day4,
+        Day5,
+        Day6,
+        Day7,
+        Day8,
+        Day9,
+        Day10,
         Day11
     )
 }
--- aoc-2024/src/main/kotlin/ski/gagar/aoc2024/day11/Day11.kt
+++ aoc-2024/src/main/kotlin/ski/gagar/aoc2024/day11/Day11.kt
@@ -8,7 +8,7 @@ import java.math.BigInteger
 object Day11 : Puzzle {
     override val name: String = "Plutonian Pebbles"
     override fun part1(input: InputStream): Int =
-        ski.gagar.aoc2024.day11.part1.countStones(input.bufferedReader().readTextAndClose(), 25)
+        ski.gagar.aoc2024.day11.part1.countStones(input.bufferedReader().readTextAndClose())
     override fun part2(input: InputStream): BigInteger =
         ski.gagar.aoc2024.day11.part2.countStones(input.bufferedReader().readTextAndClose(), 75)
 }
