570cccd3a4373f5b9c8f672bc3b36209231e8180
Kirill Gagarski <kirill.gagarski@gmail.com>
2024-12-11 16:57:47 +0100

    correct cache for day11

--- aoc-2024/src/main/kotlin/ski/gagar/aoc2024/day11/Day11.kt
+++ aoc-2024/src/main/kotlin/ski/gagar/aoc2024/day11/Day11.kt
@@ -7,8 +7,8 @@ import java.math.BigInteger
 
 object Day11 : Puzzle {
     override val name: String = "Plutonian Pebbles"
-//    override fun part1(input: InputStream): Int =
-//        ski.gagar.aoc2024.day11.part1.countStones(input.bufferedReader().readTextAndClose(), 43)
+    override fun part1(input: InputStream): Int =
+        ski.gagar.aoc2024.day11.part1.countStones(input.bufferedReader().readTextAndClose(), 25)
     override fun part2(input: InputStream): BigInteger =
         ski.gagar.aoc2024.day11.part2.countStones(input.bufferedReader().readTextAndClose(), 75)
 }
